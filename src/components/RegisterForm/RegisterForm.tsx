9bfc9e2b4299f637db6aa82aa7088504fe283897
agatateodozja <agatachase@gmail.com>
2022-11-19 12:07:52 +0100

    Firebase add to ptoject and create RForm

--- src/components/RegisterForm/RegisterForm.tsx
+++ src/components/RegisterForm/RegisterForm.tsx
@@ -1,7 +1,11 @@
 import react from "react"
+import { auth } from "../../helpers/firebaseConfig";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import {useForm} from "react-hook-form";
+import { RegisterFormData } from "../../helpers/interfaces";
 
-const HomePage = () => {
+const RegisterForm = () => {
     return <div>Home</div>
 }
 
-export default HomePage;
+export default RegisterForm;
