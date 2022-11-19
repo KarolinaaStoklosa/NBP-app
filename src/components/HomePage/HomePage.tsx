6ac95b29ffe3b2ebf48599b67f8557f8ddf48324
KarolinaaStoklosa <michniak.k@gmail.com>
2022-11-19 11:14:11 +0100

    routing and homePage

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,7 +1,25 @@
 import react from "react"
 
 const HomePage = () => {
-    return <div>Home</div>
+    return (
+        <>
+            <h2>O NBP</h2>
+            <p>Narodowy Bank Polski (NBP) jest bankiem centralnym Rzeczypospolitej Polskiej. Wypełnia zadania określone w Konstytucji RP, ustawie o Narodowym Banku Polskim i ustawie Prawo bankowe. Wymienione akty prawne gwarantują niezależność NBP od innych organów państwa.</p>
+            <p>NBP pełni trzy podstawowe funkcje: banku emisyjnego, banku banków oraz centralnego banku państwa.</p>
+            <p>Organami Narodowego Banku Polskiego są: Prezes NBP, Rada Polityki Pieniężnej oraz Zarząd NBP.</p>
+            <p>Podstawowym zadaniem NBP jest utrzymanie stabilnego poziomu cen, przy jednoczesnym wspieraniu polityki gospodarczej Rządu, o ile nie ogranicza to podstawowego celu NBP. Zgodnie z opracowaną przez Radę Polityki Pieniężnej Strategią Polityki Pieniężnej po 2003 roku, celem NBP jest ustabilizowanie inflacji na poziomie 2,5 proc. z dopuszczalnym przedziałem wahań +/- 1 punkt procentowy.</p>
+            <p>Do głównych obszarów działalności NBP należą:</p>
+            <ul>
+                <li>prowadzenie polityki pieniężnej,</li>
+                <li>działania na rzecz stabilności krajowego systemu finansowego,</li>
+                <li>działalność emisyjna,</li>
+                <li>rozwój systemu płatniczego,</li>
+                <li>zarządzanie rezerwami dewizowymi Polski,</li>
+                <li>prowadzenie bankowej obsługi budżetu państwa,</li>
+                <li>działalność edukacyjna i informacyjna.</li>
+            </ul>
+        </>
+    )
 }
 
 export default HomePage;
